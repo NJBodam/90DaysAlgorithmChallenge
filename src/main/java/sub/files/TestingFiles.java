32b84cbde3883f285f2af0e1f1a42c8bc5ae57e2
NJBodam <bodambest@yahoo.com>
2022-04-20 13:43:21 +0100

    solved population census algo with scanner io

--- src/main/java/sub/files/TestingFiles.java
+++ src/main/java/sub/files/TestingFiles.java
@@ -3,10 +3,12 @@ package sub.files;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class TestingFiles {
@@ -16,7 +18,7 @@ public class TestingFiles {
     }
     public static void main(String[] args) throws FileNotFoundException {
         // To read lines
-        File file = new File("/Users/mac/Downloads/dataset_91007.txt");
+        File file = new File("/Users/mac/Downloads/dataset_91069.txt");
 
         // To read all contents in the file
         String pathname = "/Users/mac/Downloads/dataset_91007.txt";
@@ -31,29 +33,62 @@ public class TestingFiles {
 //            e.printStackTrace();
 //        }
 
-
         int num = 0;
         try (Scanner scanner = new Scanner(file)) {
-            List<Integer> numArr = new ArrayList<>();
-            while (scanner.hasNextInt()) {
-                numArr.add(scanner.nextInt());
+            List<Long> numArr = new ArrayList<>();
+            Map<Integer, Long> mapper = new HashMap<>();
+            // Whenever you use scanner.nextInt() even in an if block
+            scanner.nextLine();
+            long diff = 0;
+            long diffTemp = 0;
+            int year = 0;
+            int key = scanner.nextInt();
+            long value = scanner.nextLong();
+            while (scanner.hasNext()) {
+                int nKey = scanner.nextInt();
+                long nValue = scanner.nextLong();
+                diffTemp = nValue - value;
+                if (diffTemp > diff) {
+                    diff = diffTemp;
+                    year = nKey;
+                }
+                key = nKey;
+                value = nValue;
+           //     mapper.put(key, value);
+
+              // numArr.add(Long.parseLong(scanner.next()));
+               // System.err.println(scanner.nextLong());
+//                long temp = scanner.nextLong();
+//                maxi = Math.max(temp, maxi);
             }
+
+
+            System.err.println(year);
+
+//            for (int i = 1; i < numArr.size(); i++) {
+//                if(numArr.get(i) % 2 != 0) {
+//                    if(numArr.get(i + 2) > numArr.get(i)) {
+//                        System.out.println(numArr.get(i + 1));
+//                        System.out.println(numArr.get(i + 2));
+//                    }
+//                }
+//            }
             //IntStream.of(numArr).max(Comparator.naturalOrder()).getAsInt();
-            numArr.stream().max(Comparator.naturalOrder()).get();
-            System.err.println(numArr.stream().max(Comparator.naturalOrder()).get());
+         //   numArr.stream().max(Comparator.naturalOrder()).get();
+          //  System.err.println(numArr.stream().max(Comparator.naturalOrder()).get());
         } catch (FileNotFoundException e) {
             System.out.println("No file found: " + file.getName());
         }
 
-       // System.err.print(num);
-        System.out.println("File name: " + file.getName());
-        System.out.println("File path: " + file.getPath());
-        System.out.println("Is file: " + file.isFile());
-        System.out.println("Is directory: " + file.isDirectory());
-        System.out.println("Exists: " + file.exists());
-        System.out.println("Parent path: " + file.getParent());
-        System.out.println("Can execute? : " + file.canExecute());
-        System.out.println("Can write? : " + file.canWrite());
-        System.out.println("Can read? : " + file.canRead());
+        //System.err.print(num);
+//        System.out.println("File name: " + file.getName());
+//        System.out.println("File path: " + file.getPath());
+//        System.out.println("Is file: " + file.isFile());
+//        System.out.println("Is directory: " + file.isDirectory());
+//        System.out.println("Exists: " + file.exists());
+//        System.out.println("Parent path: " + file.getParent());
+//        System.out.println("Can execute? : " + file.canExecute());
+//        System.out.println("Can write? : " + file.canWrite());
+//        System.out.println("Can read? : " + file.canRead());
     }
 }
