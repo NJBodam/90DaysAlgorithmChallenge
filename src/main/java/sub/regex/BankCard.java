d89a2c8866f939b277b48f33634400f70dd46890
NJBodam <bodambest@yahoo.com>
2022-05-10 08:43:24 +0100

    BankCard Regex added

---
+++ src/main/java/sub/regex/BankCard.java
@@ -0,0 +1,41 @@
+package sub.regex;
+
+import java.util.Scanner;
+
+public class BankCard {
+    /*
+    Card name validation and identification
+
+    Write a program that uses regular expressions to check whether the input
+    card number is valid and identifies the card network name.
+
+    The input must consist only of numbers. Your program must take a string as
+    an input and print the name of the card network as output,
+    choosing from the set of names specified below, or the message: "Card number does not exist”.
+
+    Card networks: Visa, Mastercard, American Express.
+
+    A Visa card starts with 4 and has the length of 16 digits.
+    A MasterCard starts with the numbers from 51 to 55, or the numbers 2221 to 2720. All have 16 digits.
+    American Express card numbers start with 34 or 37 and have 15 digits.
+    */
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String numbers = scn.nextLine();
+        String card = numbers.replaceAll("\\s", "");
+        String visaRegex = "4\\d{15}"; // put your code here
+        String masterCardRegex = "(5[1-5]|2[2-7])\\d{14}"; // put your code here
+        String americanExpressRegex = "3(4|7)\\d{13}"; // put your code here
+
+        if (card.matches(visaRegex)) {
+            System.out.println("Visa");
+        } else if (card.matches(masterCardRegex)) {
+            System.out.println("MasterCard");
+        } else if (card.matches(americanExpressRegex)) {
+            System.out.println("AmericanExpress");
+        } else {
+            System.out.println("Card number does not exist");
+        }
+    }
+}
--- src/main/java/sub/regex/LoginChecker.java
+++ src/main/java/sub/regex/LoginChecker.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class LoginChecker {
 
     public static void main(String[] args) {
-        class CheckLoginProblem {
 
-            public static void main(String[] args) {
+        /*
+        Let's consider a program that checks whether the input string is a valid login.
+        We assume that login can contain any Latin letter, a number,
+        the underscore character _, and the dollar sign $.
+        The length must be no less than 5 characters and no more than 12 characters.
+        There is one additional feature: we should ignore all whitespace characters in
+        the beginning and the end of the input string.
+        */
 
-                /* The scanner object to read data from the standard input */
-                Scanner scanner = new Scanner(System.in);
+        /* The scanner object to read data from the standard input */
+        Scanner scanner = new Scanner(System.in);
 
-                /* The common pattern for valid logins */
-                String loginRegex = "\\s*[a-zA-Z0-9_$]{5,12}\\s*";
+        /* The common pattern for valid logins */
+        String loginRegex = "\\s*[a-zA-Z0-9_$]{5,12}\\s*";
 
-                /* The read string which may be a login */
-                String mayBeLogin = scanner.nextLine();
+        /* The read string which may be a login */
+        String mayBeLogin = scanner.nextLine();
 
-                boolean isLogin = mayBeLogin.matches(loginRegex);
+        boolean isLogin = mayBeLogin.matches(loginRegex);
 
-                System.out.println(isLogin);
-            }
-        }
+        System.out.println(isLogin);
     }
 }
