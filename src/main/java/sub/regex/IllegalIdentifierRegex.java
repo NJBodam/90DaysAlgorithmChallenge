de9016101466da32c3ecbb7d3e4d912511b0c763
NJBodam <bodambest@yahoo.com>
2022-05-14 14:55:31 +0100

    solved ilelgalIdentifierRegex

--- src/main/java/sub/regex/IllegalIdentifierRegex.java
+++ src/main/java/sub/regex/IllegalIdentifierRegex.java
@@ -18,15 +18,49 @@ public class IllegalIdentifierRegex {
         Using the provided template, write a program that reads n identifiers
         and then outputs all invalid ones in the same order as they appear in the input data.
         We hope that you will use regexes to solve the problem.
+
+        Sample Input 1:
+        6
+        ident
+        i
+        __
+        1a
+        b33
+        _n1
+
+        Sample Output 1:
+        __
+        1a
+
         */
 
         Scanner scanner = new Scanner(System.in);
-        String regex = "(?=.*[a-zA-Z])(?=.*[\\d]).";
-        String regex2 = "[__]";
+        String regex = "([0-9$].*)|(__.*)|_";
+        String regex1 = "_[_].*";
+        String regex2 = "[a-zA-Z_][^_]*";
+
         String reg = "";
-        while (scanner.hasNext()) {
-            String identifier = scanner.nextLine();
+        int size = scanner.nextInt();
+        String[] arr = new String[size];
+
+       // System.out.println(scanner.next().matches(regex2));
+
+        int index = 0;
+       // size -= 1;
+        while (index != size) {
+            arr[index] = scanner.next();
+            index++;
+        }
+//
+        for (String s : arr) {
+            if(s.matches(regex)) {
+                System.err.println(s);;
+            }
 
         }
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if(arr[i].charAt(0) == "") System.out.println(arr[i]);;
+//        }
     }
 }
